58c07c52e98e83654e24cd7fbf9c21f5d171e96b
jaaw1 <78907301+jaaw1@users.noreply.github.com>
2021-12-16 15:19:08 +0200

    Update settings.py
    SECRET_KEY = removed when going public. Owner has it.

--- gameloanguru/settings.py
+++ gameloanguru/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rz_&&ym3o54q(&3d$fxgahdmx)+=0azzf6m$mafhp7#-8_4(ho'
+"""SECRET_KEY = JAANI COPIED THIS FOR SAFETY REASONS WHEN MAKING PUBLIC"""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
